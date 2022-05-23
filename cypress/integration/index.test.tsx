51caeaf60909486403fed45639408ac437e2904c
Chris Evans <chris@ceva24.dev>
2022-05-23 21:06:16 +0100

    test: update Cypress tests

--- cypress/integration/index.test.tsx
+++ cypress/integration/index.test.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unassigned-import */
 import "@percy/cypress";
+import "@testing-library/cypress/add-commands";
 
 describe("index page", () => {
     it("renders", () => {
@@ -8,9 +9,48 @@ describe("index page", () => {
         cy.percySnapshot();
     });
 
-    it("has the correct page title", () => {
+    it("shows a list of classes in the class select list", () => {
+        const characterClasses = [
+            "None",
+            "Brute",
+            "Scoundrel",
+            "Spellweaver",
+            "Tinkerer",
+            "Mindthief",
+            "Cragheart",
+        ];
+
         cy.visit("/");
 
-        cy.title().should("equal", "Gloomhaven Character Planner");
+        cy.findByRole("button", { name: "Class" }).click();
+
+        const characterClassList = cy.findByRole("listbox", { name: "Class" });
+        const characterClassOptions =
+            characterClassList.findAllByRole("option");
+
+        characterClassOptions.should("have.length", 7);
+
+        characterClassOptions.each((characterClass, index) => {
+            cy.wrap(characterClass).should(
+                "have.text",
+                characterClasses[index]
+            );
+        });
+    });
+
+    it("shows the class mat after selecting a class", () => {
+        cy.visit("/");
+
+        cy.findByRole("img", { name: "Character mat" }).should("not.exist");
+
+        cy.findByRole("button", { name: "Class" }).click();
+
+        cy.findByRole("listbox", { name: "Class" })
+            .findByRole("option", { name: "Spellweaver" })
+            .click();
+
+        cy.findByRole("img", { name: "Character mat" }).should("exist");
+
+        cy.percySnapshot();
     });
 });
