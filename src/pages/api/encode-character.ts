be08547b1de899678bdbee10a3818983f38581b7
Chris Evans <chris@ceva24.dev>
2022-06-24 00:09:26 +0100

    feat: add api route to serialize and encode character data

---
+++ src/pages/api/encode-character.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { serializeCharacterData } from "../../data/data-serializer";
+import { encodeShareableLinkData } from "../../data/link-codec";
+
+const handler = (request: NextApiRequest, response: NextApiResponse) => {
+    if (request.method !== "POST") {
+        response.status(405).json({ message: "405 Method Not Allowed" });
+        return;
+    }
+
+    try {
+        const character: Character = request.body as Character;
+
+        console.log(
+            `Received request to encode character '${JSON.stringify(
+                character
+            )}'`
+        );
+
+        const serializedCharacterData = serializeCharacterData(character);
+
+        const encodedCharacterData = encodeShareableLinkData(
+            serializedCharacterData
+        );
+
+        console.log(`Encoded character data as '${encodedCharacterData}'`);
+
+        response.status(200).json({ encodedCharacterData });
+    } catch (error: unknown) {
+        console.error(error);
+
+        if (error instanceof TypeError) {
+            response.status(400).json({ message: "400 Bad Request" });
+        } else {
+            response.status(500).json({ message: "500 Internal Server Error" });
+        }
+    }
+};
+
+export default handler;
