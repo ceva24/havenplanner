db12dff25fcc3c751a8828ac30ef88ed0e0c633a
Chris Evans <chris@ceva24.dev>
2022-06-25 02:39:08 +0100

    refactor: use absolute paths in cypress tests (#69)

--- cypress/integration/character-mat.test.tsx
+++ cypress/integration/character-mat.test.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unassigned-import */
 import "@testing-library/cypress/add-commands";
-import "../support/commands";
+import "@/support/commands";
 
 describe("character mat", () => {
     it("shows the character mat", () => {
