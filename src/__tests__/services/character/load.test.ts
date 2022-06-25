748fd7d88299cb8ac2116aaf0710da0206c3ce8d
Chris Evans <chris@ceva24.dev>
2022-06-25 14:36:00 +0100

    test: add character service tests

---
+++ src/__tests__/services/character/load.test.ts
@@ -0,0 +1,69 @@
+import { loadCharacter } from "@/services/character/load";
+import { characterClasses } from "@/utils/constants";
+import * as dataSerializer from "@/utils/data-serializer";
+import * as linkCodec from "@/utils/link-codec";
+
+const character: Character = {
+    name: "Test character",
+    experience: 50,
+    gold: 12,
+    notes: "Test",
+    characterClass: characterClasses[3],
+};
+
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
+describe("load character service", () => {
+    it("decodes and deserializes character data", () => {
+        const character: Character = {
+            name: "Test character",
+            experience: 50,
+            gold: 12,
+            notes: "Test",
+            characterClass: characterClasses[3],
+        };
+
+        jest.spyOn(linkCodec, "decode").mockImplementationOnce(() => "decodedData");
+        jest.spyOn(dataSerializer, "deserialize").mockImplementationOnce(() => character);
+
+        const result: Character = loadCharacter("test");
+
+        expect(linkCodec.decode).toHaveBeenCalledTimes(1);
+        expect(linkCodec.decode).toHaveBeenCalledWith("test");
+
+        expect(dataSerializer.deserialize).toHaveBeenCalledTimes(1);
+        expect(dataSerializer.deserialize).toHaveBeenCalledWith("decodedData");
+
+        expect(result).toBe(character);
+    });
+
+    it("decodes character data", () => {
+        jest.spyOn(linkCodec, "decode").mockImplementationOnce(() => "");
+
+        loadCharacter("test");
+
+        expect(linkCodec.decode).toHaveBeenCalledTimes(1);
+        expect(linkCodec.decode).toHaveBeenCalledWith("test");
+    });
+
+    it("deserializes character data", () => {
+        jest.spyOn(linkCodec, "decode").mockImplementationOnce(() => "decodedData");
+        jest.spyOn(dataSerializer, "deserialize").mockImplementationOnce(() => character);
+
+        loadCharacter("test");
+
+        expect(dataSerializer.deserialize).toHaveBeenCalledTimes(1);
+        expect(dataSerializer.deserialize).toHaveBeenCalledWith("decodedData");
+    });
+
+    it("returns the deserialized character", () => {
+        jest.spyOn(linkCodec, "decode").mockImplementationOnce(() => "");
+        jest.spyOn(dataSerializer, "deserialize").mockImplementationOnce(() => character);
+
+        const result: Character = loadCharacter("test");
+
+        expect(result).toBe(character);
+    });
+});
