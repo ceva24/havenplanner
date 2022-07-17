7c155ed2c8ba09e6f5730675b6c0af96cb57b9cb
Chris Evans <chris@ceva24.dev>
2022-07-17 22:16:06 +0100

    test: fix cypress index tests

--- cypress/e2e/index.spec.tsx
+++ cypress/e2e/index.spec.tsx
@@ -33,18 +33,14 @@ describe("index page", () => {
     });
 
     it("loads character details from the query string", () => {
-        cy.visit(
-            "/?character=Cx6AeyJuIjoiVGVzdCBDaGFyYWN0ZXIiLCJ4IjoyNDAsImciOjc1LCJkIjoiSXQncyBhIHRlc3QiLCJjIjoyfQM="
-        );
+        cy.visit("/?character=GzUA%2BI3UYs0Z7oTgttSXH85bFZOCWMyCWCw64JDTb6EEYIEn1MoD9lHonqMwMp7FNWkxjTAsdlv%2F");
 
         cy.findSelectClassButton().should("have.text", "Spellweaver");
         cy.findNameField().should("have.value", "Test Character");
     });
 
     it("clears the query string after loading character details from it", () => {
-        cy.visit(
-            "/?character=Cx6AeyJuIjoiVGVzdCBDaGFyYWN0ZXIiLCJ4IjoyNDAsImciOjc1LCJkIjoiSXQncyBhIHRlc3QiLCJjIjoyfQM="
-        );
+        cy.visit("/?character=GzUA%2BI3UYs0Z7oTgttSXH85bFZOCWMyCWCw64JDTb6EEYIEn1MoD9lHonqMwMp7FNWkxjTAsdlv%2F");
 
         cy.location("search").should("equal", "");
     });
