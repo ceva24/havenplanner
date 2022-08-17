f97dd500451470a454cf4c0dacf112e7bd580dd0
Chris Evans <chris@ceva24.dev>
2022-08-17 02:28:13 +0100

    test: add e2e test for selectable ability cards

--- cypress/e2e/ability-cards.spec.tsx
+++ cypress/e2e/ability-cards.spec.tsx
@@ -29,4 +29,57 @@ describe("ability cards tab", () => {
 
         cy.findByRole("region", { name: "Level X Ability Cards" }).findAllByRole("img").should("have.length", 3);
     });
+
+    it("renders level 2-9 cards", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.findByRole("region", { name: "Level 2 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 3 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 4 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 5 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 6 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 7 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 8 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+        cy.findByRole("region", { name: "Level 9 Ability Cards" }).findAllByRole("img").should("have.length", 2);
+    });
+
+    it("renders cards as locked by default", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.findByRole("checkbox", { name: "Fatal Advance" }).should("have.attr", "aria-checked", "false");
+    });
+
+    it("unlocks a card when clicking on it", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.findByRole("checkbox", { name: "Fatal Advance" }).click().should("have.attr", "aria-checked", "true");
+    });
+
+    it("unlocks a card when pressing space", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.findByRole("checkbox", { name: "Fatal Advance" })
+            .focus()
+            .type(" ")
+            .should("have.attr", "aria-checked", "true");
+    });
+
+    it("unlocks a card when pressing enter", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.findByRole("checkbox", { name: "Fatal Advance" })
+            .focus()
+            .type("{enter}")
+            .should("have.attr", "aria-checked", "true");
+    });
 });
