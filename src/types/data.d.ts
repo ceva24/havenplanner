8b9d3c0eae7861bc378d83d516acbd5f14077a5e
Chris Evans <chris@ceva24.dev>
2023-01-02 14:38:18 +0000

    refactor: move asset types to separate d.ts file

---
+++ src/types/data.d.ts
@@ -0,0 +1,35 @@
+interface Asset {
+    id: number;
+    name: string;
+    imageUrl: string;
+}
+
+interface CharacterClass extends Asset {
+    characterMatFrontImageUrl: string;
+    characterMatBackImageUrl: string;
+    cardBackImageUrl: string;
+    handSize: number;
+    abilityCards: AbilityCard[];
+    perks: Perk[];
+}
+
+interface PersonalQuest extends Asset {}
+
+interface AbilityCard extends Asset {
+    level: string;
+}
+
+interface Enhancement extends Asset {}
+
+interface Perk {
+    description: string;
+    count: number;
+    add: AttackModifierCard[];
+    remove: AttackModifierCard[];
+}
+
+interface AttackModifierCard extends Asset {}
+
+interface Item extends Asset {
+    slot: string;
+}
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -19,69 +19,20 @@ interface Character {
     items: CharacterItem[];
 }
 
-interface SerializedCharacter {
-    n: string; // Name
-    x: number; // Experience
-    g: number; // Gold
-    d: string; // Notes
-    c: number; // Character id
-    q?: number; // Personal quest id
-    i: number[]; // Item ids
-    u: number[]; // Unlocked ability card ids
-    h: number[]; // Hand ability card ids
-    p: Array<[number, number]>; // Gained perk and checkbox indices
-    b: boolean[][]; // Gained battle goal checkmark groups
-}
-
-interface CharacterItem {
-    id: string;
-    item: Item;
-}
-
-interface Asset {
-    id: number;
-    name: string;
-    imageUrl: string;
-}
-
-interface CharacterClass extends Asset {
-    characterMatFrontImageUrl: string;
-    characterMatBackImageUrl: string;
-    cardBackImageUrl: string;
-    handSize: number;
-    abilityCards: AbilityCard[];
-    perks: Perk[];
-}
-
-interface PersonalQuest extends Asset {}
-
-interface Item extends Asset {
-    slot: string;
-}
-
-interface AbilityCard extends Asset {
-    level: string;
-}
-
-interface AttackModifierCard extends Asset {}
-
-interface Enhancement extends Asset {}
-
-interface AttackModifierDeckCard {
-    card: AttackModifierCard;
-    count: number;
-}
-
-interface Perk {
-    description: string;
-    count: number;
-    add: AttackModifierCard[];
-    remove: AttackModifierCard[];
+interface GainedEnhancement {
+    abilityCard: AbilityCard;
+    slot: number;
+    enhancement: Enhancement;
 }
 
 interface GainedPerk {
     perk: Perk;
     checkboxIndex: number;
+}
+
+interface AttackModifierDeckCard {
+    card: AttackModifierCard;
+    count: number;
 }
 
 interface BattleGoalCheckmarkGroup {
@@ -94,8 +45,21 @@ interface BattleGoalCheckmark {
     value: boolean;
 }
 
-interface GainedEnhancement {
-    abilityCard: AbilityCard;
-    slot: number;
-    enhancement: Enhancement;
+interface CharacterItem {
+    id: string;
+    item: Item;
+}
+
+interface SerializedCharacter {
+    n: string; // Name
+    x: number; // Experience
+    g: number; // Gold
+    d: string; // Notes
+    c: number; // Character id
+    q?: number; // Personal quest id
+    i: number[]; // Item ids
+    u: number[]; // Unlocked ability card ids
+    h: number[]; // Hand ability card ids
+    p: Array<[number, number]>; // Gained perk and checkbox indices
+    b: boolean[][]; // Gained battle goal checkmark groups
 }
