16c3aaa35f08e78b1f862c7c1096fbc9c6c14a1e
Chris Evans <chris@ceva24.dev>
2023-01-21 21:41:49 +0000

    test: add e2e tests for enhanced ability card images

--- cypress/e2e/ability-cards/enhancements.spec.tsx
+++ cypress/e2e/ability-cards/enhancements.spec.tsx
@@ -69,4 +69,123 @@ describe("ability cards tab - enhancements", () => {
             .should("have.attr", "src")
             .should("include", "enhancement-icons/gloomhaven/poison");
     });
+
+    it("updates the ability card image to show the enhanced card when selecting enhancements", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.selectTab("Enhancements");
+
+        cy.findEnhancementsAutocomplete("Trample", "Attack", 0).click();
+        cy.findByRole("option", { name: "POISON" }).click();
+
+        cy.findEnhancementsAutocomplete("Trample", "PIERCE", 0).click();
+        cy.findByRole("option", { name: "+1" }).click();
+
+        cy.findEnhancementsAutocomplete("Trample", "Move", 0).click();
+        cy.findByRole("option", { name: "Jump" }).click();
+
+        cy.findEnhancementsAutocomplete("Trample", "Attack", 1).click();
+        cy.findByRole("option", { name: "Fire" }).click();
+
+        cy.findByRole("img", { name: "Trample" })
+            .should("have.attr", "src")
+            .should("include", "/gh-trample/poison-plus-one-jump-fire-none");
+    });
+
+    it("shows enhanced ability card images in the deck", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.selectTab("Enhancements");
+
+        cy.findEnhancementsAutocomplete("Trample", "Attack", 0).click();
+        cy.findByRole("option", { name: "POISON" }).click();
+
+        cy.selectTab("Deck");
+
+        cy.findByRole("img", { name: "Trample" })
+            .should("have.attr", "src")
+            .should("include", "/gh-trample/poison-none-none-none-none");
+    });
+
+    it("shows enhanced ability card images in the deck for locked cards", () => {
+        cy.visit("/");
+
+        cy.findExperienceField().type("120");
+
+        cy.selectTab("Ability Cards");
+
+        cy.selectTab("Enhancements");
+
+        cy.findEnhancementsAutocomplete("Fatal Advance", "Move", 0).click();
+        cy.findByRole("option", { name: "+1" }).click();
+
+        cy.selectTab("Deck");
+
+        cy.findByRole("img", { name: "Fatal Advance" })
+            .should("have.attr", "src")
+            .should("include", "/gh-fatal-advance/plus-one");
+    });
+
+    it("shows enhanced ability card images in the deck for disabled cards", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.selectTab("Enhancements");
+
+        cy.findEnhancementsAutocomplete("Fatal Advance", "Move", 0).click();
+        cy.findByRole("option", { name: "+1" }).click();
+
+        cy.selectTab("Deck");
+
+        cy.findByRole("img", { name: "Fatal Advance" })
+            .should("have.attr", "src")
+            .should("include", "/gh-fatal-advance/plus-one");
+    });
+
+    it("shows enhanced ability card images in the edit hand dialog", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.selectTab("Enhancements");
+
+        cy.findEnhancementsAutocomplete("Trample", "Attack", 0).click();
+        cy.findByRole("option", { name: "POISON" }).click();
+
+        cy.selectTab("Hand");
+
+        cy.findEditHandButton().click();
+
+        cy.findByRole("img", { name: "Trample" })
+            .should("have.attr", "src")
+            .should("include", "/gh-trample/poison-none-none-none-none");
+    });
+
+    it("shows enhanced ability card images in the hand", () => {
+        cy.visit("/");
+
+        cy.selectTab("Ability Cards");
+
+        cy.selectTab("Enhancements");
+
+        cy.findEnhancementsAutocomplete("Trample", "Attack", 0).click();
+        cy.findByRole("option", { name: "POISON" }).click();
+
+        cy.selectTab("Hand");
+
+        cy.findEditHandButton().click();
+
+        cy.findActiveAbilityCard("Trample").click();
+
+        cy.clickCloseButton();
+
+        cy.findByRole("img", { name: "Trample" })
+            .should("have.attr", "src")
+            .should("include", "/gh-trample/poison-none-none-none-none");
+    });
 });
