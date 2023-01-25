6cdab5496980e896203cccac461b277b11d4f065
Chris Evans <chris@ceva24.dev>
2023-01-25 23:40:42 +0000

    fix: improve responsive styles for ability cards and tabs (#196)
    * fix: use correct aspect ratios for ability cards
    
    * fix: render ability cards page tabs correctly on smaller screens

--- src/components/core/cards.tsx
+++ src/components/core/cards.tsx
@@ -6,40 +6,31 @@ interface CardProps {
 }
 
 const Card = ({ src, altText }: CardProps) => {
-    return (
-        <Image
-            webpPath={src}
-            fallbackImageType="jpg"
-            altText={altText}
-            width={240}
-            height={320}
-            style={{ maxWidth: "100%", borderRadius: "3%" }}
-        />
-    );
+    return <CardImage src={src} altText={altText} width={240} height={320} />;
 };
 
 const SmallCard = ({ src, altText }: CardProps) => {
-    return (
-        <Image
-            webpPath={src}
-            fallbackImageType="jpg"
-            altText={altText}
-            width={165}
-            height={244}
-            style={{ borderRadius: "3%" }}
-        />
-    );
+    return <CardImage src={src} altText={altText} width={165} height={244} />;
 };
 
 const WideCard = ({ src, altText }: CardProps) => {
+    return <CardImage src={src} altText={altText} width={200} height={150} />;
+};
+
+interface CardImageProps extends CardProps {
+    width: number;
+    height: number;
+}
+
+const CardImage = ({ src, altText, width, height }: CardImageProps) => {
     return (
         <Image
             webpPath={src}
             fallbackImageType="jpg"
             altText={altText}
-            width={200}
-            height={150}
-            style={{ borderRadius: "3%" }}
+            width={width}
+            height={height}
+            style={{ borderRadius: "3%", minWidth: width, minHeight: height }}
         />
     );
 };
