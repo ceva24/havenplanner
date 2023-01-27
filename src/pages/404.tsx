037c75497c4f479a9ef180b05761e2b2c3460306
Chris Evans <chris@ceva24.dev>
2023-01-27 00:24:06 +0000

    chore: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import ErrorTemplate from "@/components/error/error-template";
+
+const NotFoundPage = () => {
+    return <ErrorTemplate message="Page not found" />;
+};
+
+export default NotFoundPage;
