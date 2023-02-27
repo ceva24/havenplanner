bb87bc75bf9027d632d40f84727fec5ee3e99697
Chris Evans <chris@ceva24.dev>
2023-02-27 02:17:28 +0000

    fix: improve responsiveness of prosperity slider

--- src/components/settings/prosperity-slider.tsx
+++ src/components/settings/prosperity-slider.tsx
@@ -15,7 +15,7 @@ const ProsperitySlider = () => {
             <Typography id="input-slider">Prosperity</Typography>
             <Slider
                 id="prosperity-slider"
-                sx={{ width: 300 }}
+                sx={{ width: { xs: "90%", sm: "80% " } }}
                 aria-labelledby="input-slider"
                 valueLabelDisplay="off"
                 step={1}
