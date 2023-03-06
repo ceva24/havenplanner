0de5adb7655a19e3a5f99d3943c20963ff642b65
Chris Evans <chris@ceva24.dev>
2023-03-06 23:13:27 +0000

    test: fix index page unit tests

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -4,6 +4,7 @@ import type { GetServerSidePropsContext } from "next";
 import { createMocks } from "node-mocks-http";
 import Index, { getServerSideProps, type IndexProps } from "@/pages/index";
 import * as encoderService from "@/services/share/codec";
+import * as settingsService from "@/services/settings";
 import { createTestSettings, createTestCharacter } from "@/test/create-test-fixtures";
 
 jest.mock("next/router", () => {
@@ -17,6 +18,14 @@ jest.mock("@/services/share/codec", () => {
     return {
         __esModule: true,
         ...jest.requireActual("@/services/share/codec"),
+    };
+});
+
+jest.mock("@/services/settings", () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return {
+        __esModule: true,
+        ...jest.requireActual("@/services/settings"),
     };
 });
 
@@ -46,20 +55,59 @@ describe("index page", () => {
 });
 
 describe("getServerSideProps", () => {
-    it("returns the default settings", async () => {
+    it("returns the default settings when there is no save data to load", async () => {
+        const getDefaultSettingsImplementation = jest.fn();
+
+        jest.spyOn(settingsService, "getDefaultSettings").mockImplementationOnce(() => {
+            getDefaultSettingsImplementation();
+            return settings;
+        });
+
         const data = (await getServerSideProps(createMockContext({}))) as { props: IndexProps };
 
-        expect(data.props.initialSettings).toBeTruthy();
+        expect(getDefaultSettingsImplementation).toHaveBeenCalledTimes(1);
+        expect(data.props.initialSettings).toEqual(settings);
+    });
+
+    it("returns no loaded character or spoiler settings when there is no save data to load", async () => {
+        const data = (await getServerSideProps(createMockContext({}))) as { props: IndexProps };
+
+        expect(data.props.loadedCharacter).toBeFalsy();
+        expect(data.props.loadedSpoilerSettings).toBeFalsy();
     });
 
     it("loads character details from the query string parameter", async () => {
-        jest.spyOn(encoderService, "decode").mockReturnValueOnce(character);
+        jest.spyOn(encoderService, "decode").mockReturnValueOnce({ character, gameData: settings.gameData });
+        jest.spyOn(settingsService, "getSettingsForGame").mockReturnValueOnce(settings);
 
         const context: GetServerSidePropsContext = createMockContext({ character: "abc" });
 
         const data = (await getServerSideProps(context)) as { props: IndexProps };
 
         expect(data.props.loadedCharacter).toEqual(character);
+    });
+
+    it("loads spoiler settings based on the save data in the query string", async () => {
+        jest.spyOn(encoderService, "decode").mockReturnValueOnce({ character, gameData: settings.gameData });
+        jest.spyOn(settingsService, "getSpoilerSettingsForCharacter").mockReturnValueOnce(settings.spoilerSettings);
+        jest.spyOn(settingsService, "getSettingsForGame").mockReturnValueOnce(settings);
+
+        const context: GetServerSidePropsContext = createMockContext({ character: "abc" });
+
+        const data = (await getServerSideProps(context)) as { props: IndexProps };
+
+        expect(data.props.loadedSpoilerSettings).toEqual(settings.spoilerSettings);
+    });
+
+    it("creates initial based on the save data in the query string", async () => {
+        jest.spyOn(encoderService, "decode").mockReturnValueOnce({ character, gameData: settings.gameData });
+        jest.spyOn(settingsService, "getSettingsForGame").mockReturnValueOnce(settings);
+
+        const context: GetServerSidePropsContext = createMockContext({ character: "abc" });
+
+        const data = (await getServerSideProps(context)) as { props: IndexProps };
+
+        expect(data.props.initialSettings).toEqual(settings);
     });
 
     it("returns a default character if loading the character from the query string parameter fails", async () => {
