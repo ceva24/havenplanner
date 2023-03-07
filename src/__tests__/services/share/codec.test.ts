b2a2b1ae1e883638d4f56a37f6d0125d4b95f13e
Chris Evans <chris@ceva24.dev>
2023-03-07 00:54:47 +0000

    test: fix (de)serializer unit tests and some codec unit tests

--- src/__tests__/services/share/codec.test.ts
+++ src/__tests__/services/share/codec.test.ts
@@ -1,109 +1,15 @@
 import lzbase62 from "lzbase62";
-import { decode, encode } from "@/services/share/codec";
+import * as gameService from "@/services/games/game";
 import * as serializerService from "@/services/share/serializer";
 import * as deserializerService from "@/services/share/deserializer";
-import { characterClasses } from "@/loaders/character-classes";
-import { items } from "@/loaders/items";
-import { enhancements } from "@/loaders/enhancements";
-import { createTestCharacter } from "@/test/create-test-fixtures";
+import { decode, encode, type SaveData } from "@/services/share/codec";
+import { createTestCharacter, createTestSettings } from "@/test/create-test-fixtures";
 
-const character: Character = createTestCharacter({
-    name: "Test character",
-    experience: 50,
-    gold: 12,
-    notes: "Test",
-    characterClass: characterClasses[3],
-    battleGoalCheckmarkGroups: [],
-});
-
-const characterWithUnicodeCharacters = createTestCharacter({
-    name: "テストキャラクター",
-    experience: 50,
-    gold: 12,
-    notes: "テスト",
-    characterClass: characterClasses[3],
-    battleGoalCheckmarkGroups: [],
-});
-
-const characterWithItems = createTestCharacter({
-    name: "Test character",
-    experience: 50,
-    gold: 12,
-    notes: "Test",
-    characterClass: characterClasses[3],
-    battleGoalCheckmarkGroups: [],
-    items: [
-        { id: "abc", item: items[0] },
-        { id: "def", item: items[3] },
-    ],
-});
-
-const characterWithUnlockedAbilityCards: Character = createTestCharacter({
-    name: "Test character",
-    experience: 500,
-    gold: 12,
-    notes: "Test",
-    characterClass: characterClasses[3],
-    unlockedAbilityCards: [characterClasses[3].abilityCards[12], characterClasses[3].abilityCards[13]],
-    battleGoalCheckmarkGroups: [],
-});
-
-const characterWithHand: Character = createTestCharacter({
-    characterClass: characterClasses[3],
-    hand: [characterClasses[3].abilityCards[12], characterClasses[3].abilityCards[13]],
-    battleGoalCheckmarkGroups: [],
-});
-
-const characterWithGainedEnhancements: Character = createTestCharacter({
-    characterClass: characterClasses[3],
-    gainedEnhancements: [
-        {
-            abilityCard: character.characterClass.abilityCards[0],
-            enhancementSlot: character.characterClass.abilityCards[0].enhancementSlots[0],
-            enhancement: enhancements[1],
-        },
-        {
-            abilityCard: character.characterClass.abilityCards[1],
-            enhancementSlot: character.characterClass.abilityCards[1].enhancementSlots[1],
-            enhancement: enhancements[0],
-        },
-    ],
-});
-
-const characterWithGainedPerks: Character = createTestCharacter({
-    characterClass: characterClasses[3],
-    gainedPerks: [
-        { perk: characterClasses[3].perks[1], checkboxIndex: 0 },
-        { perk: characterClasses[3].perks[0], checkboxIndex: 1 },
-    ],
-    battleGoalCheckmarkGroups: [],
-});
-
-const characterWithBattleGoalCheckmarks: Character = createTestCharacter({
-    characterClass: characterClasses[3],
-    battleGoalCheckmarkGroups: [
-        {
-            id: 0,
-            checkmarks: [
-                { id: 0, value: true },
-                { id: 1, value: true },
-                { id: 2, value: true },
-            ],
-        },
-        {
-            id: 1,
-            checkmarks: [
-                { id: 0, value: true },
-                { id: 1, value: false },
-                { id: 2, value: false },
-            ],
-        },
-    ],
-});
-
-jest.mock("uuid", () => {
+jest.mock("@/services/games/game", () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return {
-        v4: jest.fn().mockReturnValueOnce("abc").mockReturnValueOnce("def"),
+        __esModule: true,
+        ...jest.requireActual("@/services/games/game"),
     };
 });
 
@@ -127,21 +33,24 @@ beforeEach(() => {
     jest.clearAllMocks();
 });
 
-describe.skip("encode", () => {
+const character: Character = createTestCharacter();
+const settings: Settings = createTestSettings();
+
+describe("encode", () => {
     it("serializes character data", () => {
         jest.spyOn(serializerService, "serialize").mockReturnValueOnce("");
 
-        encode(character);
+        encode({ character, gameData: settings.gameData });
 
         expect(serializerService.serialize).toHaveBeenCalledTimes(1);
-        expect(serializerService.serialize).toHaveBeenCalledWith(character);
+        expect(serializerService.serialize).toHaveBeenCalledWith(character, settings.gameData);
     });
 
     it("compresses character data", () => {
         jest.spyOn(serializerService, "serialize").mockReturnValueOnce("serializedCharacter");
         jest.spyOn(lzbase62, "compress").mockReturnValueOnce("");
 
-        encode(character);
+        encode({ character, gameData: settings.gameData });
 
         expect(lzbase62.compress).toHaveBeenCalledTimes(1);
         expect(lzbase62.compress).toHaveBeenCalledWith("serializedCharacter");
@@ -150,29 +59,16 @@ describe.skip("encode", () => {
     it("returns the serialized and compressed character data", () => {
         jest.spyOn(lzbase62, "compress").mockReturnValueOnce("123");
 
-        const encodedData = encode(character);
+        const encodedData = encode({ character, gameData: settings.gameData });
 
         expect(encodedData).toEqual("123");
     });
-
-    it.each`
-        description                                     | character                            | encodedData
-        ${"character data"}                             | ${character}                         | ${"uDriterisSritEVjkrgtTYRiRTkVirisEriuA2VsNI2HtX2HsJK2HtUxjHxZDtT2LsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHGtSxHEuF"}
-        ${"character data with unicode characters"}     | ${characterWithUnicodeCharacters}    | ${"uDriterisSriwMGZhYIZVYjZBaXYnYUarisEriuA2QsNI2HtX2HsJK2HtUxeGxYDtT2KsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHGtSxHEuF"}
-        ${"character data with items"}                  | ${characterWithItems}                | ${"uDriterisSritEVjkrgtTYRiRTkVirisEriuA2VsNI2HtX2HsJK2HtUxjHxZDtT2LsM2GtZ2GtLsJEMtN2KtlxKDxHDtYxHGtVxHGtgxHGtSxHEuF"}
-        ${"character data with unlocked ability cards"} | ${characterWithUnlockedAbilityCards} | ${"uDriterisSritEVjkrgtTYRiRTkVirisEriuA2VsNII2ItX2IsJK2HtUxkHxaDtT2LsM2GtZ2GtLN2HtlxHDsM2gsMLxMDtYxTGtVxHGtgxHGtSxHEuF"}
-        ${"character data with hand"}                   | ${characterWithHand}                 | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHDsMKEMLxMDtVxTGtgxHGtSxHEuF"}
-        ${"character data with gained enhancements"}    | ${characterWithGainedEnhancements}   | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHGtVxHDtLsL222CsJ2O2JsJ2H2LtNxYDtgxfGtSxfEtWRcjVsExGLxqDxU9xUltNuF"}
-        ${"character data with gained perks"}           | ${characterWithGainedPerks}          | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHDtLsJEI2LtL3FsJtNxSDtSxZEuF"}
-        ${"character data with battle goal checkmarks"} | ${characterWithBattleGoalCheckmarks} | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHGtSxHDtLkilVsExFJ2WxRGtWRcjxGHtNNuF"}
-    `("serializes and compresses $description", ({ character, encodedData }) => {
-        expect(encode(character)).toEqual(encodedData);
-    });
 });
 
-describe.skip("decode", () => {
+describe("decode", () => {
     it("decompresses character data", () => {
-        jest.spyOn(lzbase62, "decompress").mockReturnValueOnce("");
+        jest.spyOn(lzbase62, "decompress").mockReturnValueOnce(JSON.stringify(character));
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
         jest.spyOn(deserializerService, "deserialize").mockReturnValueOnce(character);
 
         decode("test");
@@ -181,35 +77,25 @@ describe.skip("decode", () => {
         expect(lzbase62.decompress).toHaveBeenCalledWith("test");
     });
 
-    it("deserializes character data", () => {
-        jest.spyOn(lzbase62, "decompress").mockReturnValueOnce("decompressedCharacter");
+    it("looks up game data and deserializes character data", () => {
+        jest.spyOn(lzbase62, "decompress").mockReturnValueOnce(JSON.stringify(character));
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
         jest.spyOn(deserializerService, "deserialize").mockReturnValueOnce(character);
 
         decode("test");
 
         expect(deserializerService.deserialize).toHaveBeenCalledTimes(1);
-        expect(deserializerService.deserialize).toHaveBeenCalledWith("decompressedCharacter");
+        expect(deserializerService.deserialize).toHaveBeenCalledWith(character, settings.gameData);
     });
 
-    it("returns the deserialized character", () => {
+    it("returns the deserialized character and game data", () => {
+        jest.spyOn(lzbase62, "decompress").mockReturnValueOnce(JSON.stringify(character));
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
         jest.spyOn(deserializerService, "deserialize").mockReturnValueOnce(character);
 
-        const result: Character = decode("test");
+        const result: SaveData = decode("test");
 
-        expect(result).toBe(character);
-    });
-
-    it.each`
-        description                                     | encodedData                                                                                                                                                              | character
-        ${"character data"}                             | ${"uDriterisSritEVjkrgtTYRiRTkVirisEriuA2VsNI2HtX2HsJK2HtUxjHxZDtT2LsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHGtSxHEuF"}                                                           | ${character}
-        ${"character data with unicode characters"}     | ${"uDriterisSriwMGZhYIZVYjZBaXYnYUarisEriuA2QsNI2HtX2HsJK2HtUxeGxYDtT2KsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHGtSxHEuF"}                                                        | ${characterWithUnicodeCharacters}
-        ${"character data with items"}                  | ${"uDriterisSritEVjkrgtTYRiRTkVirisEriuA2VsNI2HtX2HsJK2HtUxjHxZDtT2LsM2GtZ2GtLsJEMtN2KtlxKDxHDtYxHGtVxHGtgxHGtSxHEuF"}                                                   | ${characterWithItems}
-        ${"character data with unlocked ability cards"} | ${"uDriterisSritEVjkrgtTYRiRTkVirisEriuA2VsNII2ItX2IsJK2HtUxkHxaDtT2LsM2GtZ2GtLN2HtlxHDsM2gsMLxMDtYxTGtVxHGtgxHGtSxHEuF"}                                                | ${characterWithUnlockedAbilityCards}
-        ${"character data with hand"}                   | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHDsMKEMLxMDtVxTGtgxHGtSxHEuF"}                                       | ${characterWithHand}
-        ${"character data with gained enhancements"}    | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHGtVxHDtLsL222CsJ2O2JsJ2H2LtNxYDtgxfGtSxfEtWRcjVsExGLxqDxU9xUltNuF"} | ${characterWithGainedEnhancements}
-        ${"character data with gained perks"}           | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHDtLsJEI2LtL3FsJtNxSDtSxZEuF"}                             | ${characterWithGainedPerks}
-        ${"character data with battle goal checkmarks"} | ${"uDriterisSrisluBrgsbtYRirisEriuA2OsJII2ItX2IsNxHDtUxdDsgtVccfrg2ftmV2r2StT2SsM2GtZ2GtLN2HtlxHGtYxHGtVxHGtgxHGtSxHDtLkilVsExFJ2WxRGtWRcjxGHtNNuF"}                     | ${characterWithBattleGoalCheckmarks}
-    `("decompresses and deserializes $description", ({ encodedData, character }) => {
-        expect(decode(encodedData)).toEqual(character);
+        expect(result.character).toEqual(character);
+        expect(result.gameData).toEqual(settings.gameData);
     });
 });
--- src/__tests__/services/share/serializer.test.ts
+++ src/__tests__/services/share/serializer.test.ts
@@ -1,206 +1,30 @@
 import { serialize } from "@/services/share/serializer";
-import { characterClasses } from "@/loaders/character-classes";
-import { items } from "@/loaders/items";
-import { personalQuests } from "@/loaders/personal-quests";
-import { enhancements } from "@/loaders/enhancements";
-import { createTestCharacter } from "@/test/create-test-fixtures";
+import { createTestCharacter, createTestItem, createTestSettings } from "@/test/create-test-fixtures";
+
+const settings: Settings = createTestSettings();
 
 describe("serializer", () => {
-    it("serializes a character", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            personalQuest: personalQuests[8],
-            battleGoalCheckmarkGroups: [],
-        });
-
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"q":518,"i":[],"u":[],"h":[],"e":[],"p":[],"b":[]}`
-        );
-    });
-
     it("omits the personal quest property when serializing a character with no personal quest id", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-        });
+        const character: Character = createTestCharacter();
 
-        const data: string = serialize(character);
+        const data: string = serialize(character, settings.gameData);
 
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[],"u":[],"h":[],"e":[],"p":[],"b":[]}`
-        );
-    });
-
-    it("serializes item data", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-            items: [
-                { id: "1", item: items[1] },
-                { id: "2", item: items[7] },
-            ],
-        });
-
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[2,8],"u":[],"h":[],"e":[],"p":[],"b":[]}`
-        );
+        expect(data).not.toMatch(/"q":/);
     });
 
     it("serializes duplicate items", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-            items: [
-                { id: "1", item: items[1] },
-                { id: "2", item: items[1] },
-            ],
-            unlockedAbilityCards: [],
-        });
+        const item: Item = createTestItem(1, "Boots of Test", "1");
+        const settings: Settings = createTestSettings();
+        settings.gameData.items = [item];
 
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[2,2],"u":[],"h":[],"e":[],"p":[],"b":[]}`
-        );
-    });
-
-    it("serializes unlocked ability cards", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-        });
-        character.unlockedAbilityCards = [character.characterClass.abilityCards[0]];
-
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[],"u":[61],"h":[],"e":[],"p":[],"b":[]}`
-        );
-    });
-
-    it("serializes the hand", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-        });
-        character.hand = [character.characterClass.abilityCards[0]];
-
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[],"u":[],"h":[61],"e":[],"p":[],"b":[]}`
-        );
-    });
-
-    it("serializes gained enhancements", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-        });
-        character.gainedEnhancements = [
-            {
-                abilityCard: character.characterClass.abilityCards[0],
-                enhancementSlot: character.characterClass.abilityCards[0].enhancementSlots[0],
-                enhancement: enhancements[1],
-            },
-            {
-                abilityCard: character.characterClass.abilityCards[1],
-                enhancementSlot: character.characterClass.abilityCards[1].enhancementSlots[1],
-                enhancement: enhancements[0],
-            },
+        const character: Character = createTestCharacter();
+        character.items = [
+            { id: "1", item },
+            { id: "2", item },
         ];
 
-        const data: string = serialize(character);
+        const data: string = serialize(character, settings.gameData);
 
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[],"u":[],"h":[],"e":[[61,0,1],[62,1,0]],"p":[],"b":[]}`
-        );
-    });
-
-    it("serializes gained perks", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [],
-        });
-        character.gainedPerks = [
-            { perk: character.characterClass.perks[0], checkboxIndex: 0 },
-            { perk: character.characterClass.perks[1], checkboxIndex: 1 },
-        ];
-
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[],"u":[],"h":[],"e":[],"p":[[0,0],[1,1]],"b":[]}`
-        );
-    });
-
-    it("serializes battle goal progress", () => {
-        const character: Character = createTestCharacter({
-            name: "Test Character",
-            experience: 240,
-            gold: 75,
-            notes: "It's a test",
-            characterClass: characterClasses[2],
-            battleGoalCheckmarkGroups: [
-                {
-                    id: 0,
-                    checkmarks: [
-                        { id: 0, value: true },
-                        { id: 1, value: true },
-                        { id: 2, value: true },
-                    ],
-                },
-                {
-                    id: 1,
-                    checkmarks: [
-                        { id: 0, value: true },
-                        { id: 1, value: false },
-                        { id: 2, value: false },
-                    ],
-                },
-            ],
-        });
-
-        const data: string = serialize(character);
-
-        expect(data).toEqual(
-            `{"n":"Test Character","x":240,"g":75,"d":"It's a test","c":3,"i":[],"u":[],"h":[],"e":[],"p":[],"b":[[true,true,true],[true,false,false]]}`
-        );
+        expect(data).toMatch(/"i":\[1,1]/);
     });
 });
