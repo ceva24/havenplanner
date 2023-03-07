b0d0de12ceeaf679dc6b946374a629209c5009c1
Chris Evans <chris@ceva24.dev>
2023-03-07 01:34:25 +0000

    test: add more codec unit tests

--- src/__tests__/services/share/codec.test.ts
+++ src/__tests__/services/share/codec.test.ts
@@ -3,7 +3,14 @@ import * as gameService from "@/services/games/game";
 import * as serializerService from "@/services/share/serializer";
 import * as deserializerService from "@/services/share/deserializer";
 import { decode, encode, type SaveData } from "@/services/share/codec";
-import { createTestCharacter, createTestSettings } from "@/test/create-test-fixtures";
+import {
+    createTestAbilityCard,
+    createTestCharacter,
+    createTestCharacterClass,
+    createTestEnhancement,
+    createTestItem,
+    createTestSettings,
+} from "@/test/create-test-fixtures";
 
 jest.mock("@/services/games/game", () => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
@@ -97,5 +104,138 @@ describe("decode", () => {
 
         expect(result.character).toEqual(character);
         expect(result.gameData).toEqual(settings.gameData);
+    });
+});
+
+describe("codec", () => {
+    it("serializes and deserializes character data", () => {
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const character: Character = createTestCharacter({
+            name: "My Gloomy Char",
+            experience: 10,
+            gold: 8,
+            notes: "Hello decode haven",
+            characterClass: createTestCharacterClass(1, "Test Brute"),
+        });
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        expect(decodedSaveData.character).toEqual(saveData.character);
+    });
+
+    it("serializes and deserializes game data", () => {
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        expect(decodedSaveData.gameData).toEqual(saveData.gameData);
+    });
+
+    it("serializes and deserializes character data with unicode characters", () => {
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const character: Character = createTestCharacter({
+            name: "テストキャラクター",
+            experience: 10,
+            gold: 8,
+            notes: "テスト",
+            characterClass: createTestCharacterClass(1, "Test Brute"),
+        });
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        expect(decodedSaveData.character).toEqual(saveData.character);
+    });
+
+    it("serializes and deserializes character data with items", () => {
+        const settings: Settings = createTestSettings();
+        settings.gameData.items = [createTestItem(2, "Boots of Test", "1"), createTestItem(8, "Cloak of Test", "1")];
+
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const character: Character = createTestCharacter({
+            items: [
+                { id: "abc", item: settings.gameData.items[0] },
+                { id: "def", item: settings.gameData.items[1] },
+            ],
+        });
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        const items: Item[] = decodedSaveData.character.items.map((characterItem: CharacterItem) => characterItem.item);
+
+        expect(items).toHaveLength(character.items.length);
+        expect(items).toContain(character.items[0].item);
+        expect(items).toContain(character.items[1].item);
+    });
+
+    it("serializes and deserializes character data with spoiler items", () => {
+        const settings: Settings = createTestSettings();
+        settings.gameData.items = [createTestItem(1, "Boots of Test", "Random Item Designs")];
+
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const character: Character = createTestCharacter({ items: [{ id: "abc", item: settings.gameData.items[0] }] });
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        expect(decodedSaveData.character.items).toHaveLength(character.items.length);
+        expect(decodedSaveData.character.items[0].item).toEqual(character.items[0].item);
+    });
+
+    it("serializes and deserializes character data with unlocked ability cards", () => {
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const character: Character = createTestCharacter();
+        character.unlockedAbilityCards = [
+            character.characterClass.abilityCards[2],
+            character.characterClass.abilityCards[3],
+        ];
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        expect(decodedSaveData.character).toEqual(saveData.character);
+        expect(decodedSaveData.character.unlockedAbilityCards).toHaveLength(2);
+    });
+
+    it("serializes and deserializes character data with a hand", () => {
+        jest.spyOn(gameService, "getGameDataById").mockReturnValueOnce(settings.gameData);
+
+        const character: Character = createTestCharacter();
+        character.hand = [character.characterClass.abilityCards[0], character.characterClass.abilityCards[1]];
+
+        const saveData: SaveData = { character, gameData: settings.gameData };
+
+        const data: string = encode(saveData);
+
+        const decodedSaveData: SaveData = decode(data);
+
+        expect(decodedSaveData.character).toEqual(saveData.character);
+        expect(decodedSaveData.character.hand).toHaveLength(2);
     });
 });
