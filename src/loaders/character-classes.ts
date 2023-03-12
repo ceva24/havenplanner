c803eb2da0806dce23ba6d0b1e1c2d1a9ee8340e
Chris Evans <chris@ceva24.dev>
2023-03-12 23:44:23 +0000

    feat: add basic NS data with dummy enhancements and empty perks

--- src/loaders/character-classes.ts
+++ src/loaders/character-classes.ts
@@ -1,11 +1,12 @@
 /* eslint-disable import/extensions */
 import br from "@/data/gloomhaven/classes/br.json";
-import sc from "@/data/gloomhaven/classes/sc.json";
-import sw from "@/data/gloomhaven/classes/sw.json";
 import ti from "@/data/gloomhaven/classes/ti.json";
-import mt from "@/data/gloomhaven/classes/mt.json";
+import sw from "@/data/gloomhaven/classes/sw.json";
+import sc from "@/data/gloomhaven/classes/sc.json";
 import ch from "@/data/gloomhaven/classes/ch.json";
+import mt from "@/data/gloomhaven/classes/mt.json";
+import ns from "@/data/gloomhaven/classes/ns.json";
 
-const characterClasses: CharacterClass[] = [br, sc, sw, ti, mt, ch];
+const characterClasses: CharacterClass[] = [br, sc, sw, ti, mt, ch, ns];
 
 export { characterClasses };
