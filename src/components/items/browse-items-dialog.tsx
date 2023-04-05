2211e9284cdd9a12bc95f052379519e58a31c894
Chris Evans <chris@ceva24.dev>
2023-04-05 17:12:32 +0100

    feat: show loading and error messages in browse items dialog

--- src/components/items/browse-items-dialog.tsx
+++ src/components/items/browse-items-dialog.tsx
@@ -16,31 +16,43 @@ interface BrowseItemsDialogProps {
 
 const BrowseItemsDialog = ({ isOpen, handleClose, character, setCharacter }: BrowseItemsDialogProps) => {
     const [settings] = useSettingsContext();
-    const { items }: UseItems = useItems(settings);
+    const { items, isLoading, isError }: UseItems = useItems(settings);
 
     return (
         <FullScreenDialog title="Browse items" isOpen={isOpen} handleClose={handleClose}>
-            <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-                {Object.entries(groupItems(items ?? [])).map((itemGroup: [string, Item[]]) => {
-                    const title: string = itemGroup[0];
-                    const items: Item[] = itemGroup[1];
-
-                    return (
-                        <ItemGroup
-                            key={title}
-                            title={title}
-                            items={items}
-                            handleClose={handleClose}
-                            character={character}
-                            setCharacter={setCharacter}
-                        />
-                    );
-                })}
-            </Box>
-            {!areItemsCompletelySpoiled(settings) && (
+            {isError ? (
                 <Box textAlign="center" marginY={3}>
-                    <Typography>Change your spoiler settings to see more items...</Typography>
+                    <Typography>Failed to load items</Typography>
                 </Box>
+            ) : isLoading ? (
+                <Box textAlign="center" marginY={3}>
+                    <Typography>Loading...</Typography>
+                </Box>
+            ) : (
+                <>
+                    <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+                        {Object.entries(groupItems(items ?? [])).map((itemGroup: [string, Item[]]) => {
+                            const title: string = itemGroup[0];
+                            const items: Item[] = itemGroup[1];
+
+                            return (
+                                <ItemGroup
+                                    key={title}
+                                    title={title}
+                                    items={items}
+                                    handleClose={handleClose}
+                                    character={character}
+                                    setCharacter={setCharacter}
+                                />
+                            );
+                        })}
+                    </Box>
+                    {!areItemsCompletelySpoiled(settings) && (
+                        <Box textAlign="center" marginY={3}>
+                            <Typography>Change your spoiler settings to see more items...</Typography>
+                        </Box>
+                    )}
+                </>
             )}
         </FullScreenDialog>
     );
