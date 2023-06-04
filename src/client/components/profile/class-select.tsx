4b3a3ddd55263497fda0ae5571c1c59dfc7b5005
Chris Evans <chris@ceva24.dev>
2023-06-04 23:26:17 +0100

    refactor: class select initial value when character classes are loading

--- src/client/components/profile/class-select.tsx
+++ src/client/components/profile/class-select.tsx
@@ -1,4 +1,4 @@
-import type { Dispatch, FC, SetStateAction } from "react";
+import type { Dispatch, SetStateAction } from "react";
 import { FormControl, InputLabel, MenuItem, Select, TextField, type SelectChangeEvent } from "@mui/material";
 import Image from "@/client/components/core/image";
 import { useSettingsContext } from "@/client/hooks/use-settings";
@@ -14,7 +14,7 @@ interface ClassSelectProps {
     setCharacter: Dispatch<SetStateAction<Character>>;
 }
 
-const ClassSelect: FC<ClassSelectProps> = ({ character, setCharacter }: ClassSelectProps) => {
+const ClassSelect = ({ character, setCharacter }: ClassSelectProps) => {
     const [settings, setSettings] = useSettingsContext();
     const { characterClassSummaries, isLoading, isError }: UseCharacterClassSummaries =
         useCharacterClassSummaries(settings);
@@ -34,61 +34,60 @@ const ClassSelect: FC<ClassSelectProps> = ({ character, setCharacter }: ClassSel
         <FormControl sx={{ margin: "1%", width: "98%" }}>
             {isError ? (
                 <TextField disabled label="Class" value="Failed to load classes" />
-            ) : isLoading ? (
-                <>
-                    <InputLabel id="select-class-label">Class</InputLabel>
-                    <Select
-                        value={character.characterClass.name}
-                        label="Class"
-                        labelId="select-class-label"
-                        onChange={handleChange}
-                    >
-                        <MenuItem key={character.characterClass.id} value={character.characterClass.name}>
-                            <Image
-                                webpPath={character.characterClass.imageUrl}
-                                fallbackImageType="png"
-                                altText={`${character.characterClass.name} Class Icon`}
-                                style={{ verticalAlign: "middle", marginRight: 10, flexShrink: 0 }}
-                                height={30}
-                                width={30}
-                                aria-hidden="true"
-                            />
-                            {character.characterClass.name}
-                        </MenuItem>
-                        <MenuItem key="loading-message" disabled value="Loading">
-                            Loading...
-                        </MenuItem>
-                    </Select>
-                </>
             ) : (
                 <>
                     <InputLabel id="select-class-label">Class</InputLabel>
-                    <Select
-                        value={character.characterClass.name}
-                        label="Class"
-                        labelId="select-class-label"
-                        onChange={handleChange}
-                    >
-                        {(characterClassSummaries ?? []).map((characterClass: CharacterClassSummary) => (
-                            <MenuItem key={characterClass.id} value={characterClass.name}>
+                    {isLoading ? (
+                        <Select
+                            value={character.characterClass.name}
+                            label="Class"
+                            labelId="select-class-label"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={character.characterClass.name}>
                                 <Image
-                                    webpPath={characterClass.imageUrl}
+                                    webpPath={character.characterClass.imageUrl}
                                     fallbackImageType="png"
-                                    altText={`${characterClass.name} Class Icon`}
+                                    altText={`${character.characterClass.name} Class Icon`}
                                     style={{ verticalAlign: "middle", marginRight: 10, flexShrink: 0 }}
                                     height={30}
                                     width={30}
                                     aria-hidden="true"
                                 />
-                                {characterClass.name}
+                                {character.characterClass.name}
                             </MenuItem>
-                        ))}
-                        {!charactersAreCompletelySpoiled(settings) && (
-                            <MenuItem key="spoiler-hint" disabled value="Spoiler hint">
-                                Change your spoiler settings to see more classes...
+                            <MenuItem key="loading-message" disabled value="Loading">
+                                Loading...
                             </MenuItem>
-                        )}
-                    </Select>
+                        </Select>
+                    ) : (
+                        <Select
+                            value={character.characterClass.name}
+                            label="Class"
+                            labelId="select-class-label"
+                            onChange={handleChange}
+                        >
+                            {(characterClassSummaries ?? []).map((characterClass: CharacterClassSummary) => (
+                                <MenuItem key={characterClass.id} value={characterClass.name}>
+                                    <Image
+                                        webpPath={characterClass.imageUrl}
+                                        fallbackImageType="png"
+                                        altText={`${characterClass.name} Class Icon`}
+                                        style={{ verticalAlign: "middle", marginRight: 10, flexShrink: 0 }}
+                                        height={30}
+                                        width={30}
+                                        aria-hidden="true"
+                                    />
+                                    {characterClass.name}
+                                </MenuItem>
+                            ))}
+                            {!charactersAreCompletelySpoiled(settings) && (
+                                <MenuItem key="spoiler-hint" disabled value="Spoiler hint">
+                                    Change your spoiler settings to see more classes...
+                                </MenuItem>
+                            )}
+                        </Select>
+                    )}
                 </>
             )}
         </FormControl>
