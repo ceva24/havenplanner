516760e17902cc463fd9fdb5f302121b37eec0d5
Chris Evans <chris@ceva24.dev>
2023-06-24 00:29:03 +0100

    feat: add SK character class (#301)

--- src/server/loaders/gloomhaven/character-classes.ts
+++ src/server/loaders/gloomhaven/character-classes.ts
@@ -5,10 +5,11 @@ import sw from "@/data/gloomhaven/classes/sw.json";
 import sc from "@/data/gloomhaven/classes/sc.json";
 import ch from "@/data/gloomhaven/classes/ch.json";
 import mt from "@/data/gloomhaven/classes/mt.json";
+import sk from "@/data/gloomhaven/classes/sk.json";
 import su from "@/data/gloomhaven/classes/su.json";
 import ns from "@/data/gloomhaven/classes/ns.json";
 import ss from "@/data/gloomhaven/classes/ss.json";
 
-const characterClasses: CharacterClass[] = [br, sc, sw, ti, mt, ch, su, ns, ss];
+const characterClasses: CharacterClass[] = [br, sc, sw, ti, mt, sk, ch, su, ns, ss];
 
 export { characterClasses };
