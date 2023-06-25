11c12bdeb0f7023a365cd6cacd1a18154f740ea2
Chris Evans <chris@ceva24.dev>
2023-06-25 14:35:53 +0100

    feat: add beta tag

--- src/client/components/header/header.tsx
+++ src/client/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { type Dispatch, type SetStateAction } from "react";
-import { AppBar, Box, Grid, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Chip, Grid, Link, Toolbar, Typography } from "@mui/material";
 import ShareButton from "@/client/components/share/share-button";
 import SettingsButton from "@/client/components/settings/settings-button";
 import SpoilAllSwitch from "@/client/components/settings/spoil-all-switch";
@@ -15,11 +15,18 @@ const Header = ({ title, character, setCharacter }: HeaderProps) => {
         <AppBar position="static">
             <Toolbar>
                 <Box sx={{ flexGrow: 1 }}>
-                    <Typography component="h1" style={{ lineHeight: 1 }}>
-                        <Link href="/" underline="none" color="textPrimary" variant="h1">
-                            {title}
-                        </Link>
-                    </Typography>
+                    <Grid container paddingY={{ xs: 1, md: 0 }}>
+                        <Grid item padding={1}>
+                            <Typography component="h1" style={{ lineHeight: 1 }}>
+                                <Link href="/" underline="none" color="textPrimary" variant="h1">
+                                    {title}
+                                </Link>
+                            </Typography>
+                        </Grid>
+                        <Grid item padding={1}>
+                            <Chip label={<Typography variant="body2">BETA</Typography>} />
+                        </Grid>
+                    </Grid>
                 </Box>
                 {character && setCharacter && (
                     <Box>
