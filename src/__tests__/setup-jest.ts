0af5f5d8a2ca797fe6ccaa7d9663dd3b3b871a28
Chris Evans <chris@ceva24.dev>
2023-06-25 20:56:00 +0100

    fix: resolve issue with regex and urls

--- src/__tests__/setup-jest.ts
+++ src/__tests__/setup-jest.ts
@@ -1,3 +1,6 @@
 /* eslint-disable import/no-unassigned-import */
 import "@testing-library/jest-dom";
 import "isomorphic-fetch";
+import * as matchers from "jest-extended";
+
+expect.extend(matchers);
