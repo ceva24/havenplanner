5a37e48dc31b099d8e8c3ff627f8da5af55d611f
Chris Evans <chris@ceva24.dev>
2025-08-13 16:41:49 +0100

    chore: remove beta tag (#356)

--- src/client/components/header/header.tsx
+++ src/client/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { type Dispatch, type SetStateAction } from "react";
-import { AppBar, Box, Chip, Grid, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Grid, Link, Toolbar, Typography } from "@mui/material";
 import ShareButton from "@/client/components/share/share-button";
 import SettingsButton from "@/client/components/settings/settings-button";
 import SpoilAllSwitch from "@/client/components/settings/spoil-all-switch";
@@ -15,18 +15,11 @@ const Header = ({ title, character, setCharacter }: HeaderProperties) => {
         <AppBar position="static">
             <Toolbar>
                 <Box sx={{ flexGrow: 1 }}>
-                    <Grid container paddingY={{ xs: 1, md: 0 }}>
-                        <Grid item padding={1}>
-                            <Typography component="h1" style={{ lineHeight: 1 }}>
-                                <Link href="/" underline="none" color="textPrimary" variant="h1">
-                                    {title}
-                                </Link>
-                            </Typography>
-                        </Grid>
-                        <Grid item padding={1}>
-                            <Chip label={<Typography variant="body2">BETA</Typography>} />
-                        </Grid>
-                    </Grid>
+                    <Typography component="h1" style={{ lineHeight: 1 }}>
+                        <Link href="/" underline="none" color="textPrimary" variant="h1">
+                            {title}
+                        </Link>
+                    </Typography>
                 </Box>
                 {character && setCharacter && (
                     <Box>
